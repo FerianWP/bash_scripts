17f4f4921f9cf007583a6c050c7f845ac871b587
FerianWP <158227255+FerianWP@users.noreply.github.com>
2024-02-12 23:00:07 -0500

    edit httpx

--- bash_outputs/httpx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Enter a target: "
-
-read target
-
-httpx-toolkit -l "$HOME/Desktop/bash_scripts/Targets/${target}/resolveddomains.txt" -t 75 -silent -sc -title -td | anew "$HOME/Desktop/bash_scripts/Targets/${target}/httpx.txt"
--- httpx.sh
+++ httpx.sh
@@ -4,4 +4,4 @@ echo "Enter a target: "
 
 read target
 
-httpx-toolkit -l "$HOME/Desktop/bash_scripts/Targets/${target}/resolveddomains.txt" -t 75 -silent -sc -title -td -nc | anew "$HOME/Desktop/bash_scripts/Targets/${target}/httpx.txt"
+httpx-toolkit -l "$HOME/Desktop/bash_scripts/Targets/${target}/resolveddomains.txt" -t 75 -silent -sc -title -td  | anew "$HOME/Desktop/bash_scripts/Targets/${target}/httpx.txt" | notify -silent -bulk -id webserver-info
